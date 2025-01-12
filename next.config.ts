6353a47665e62a3d202aefe3d1173fa31527e503
Antony Kowalski <beheaded_96@mail.ru>
2025-01-12 22:55:40 +0300

    project setup

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
-import type { NextConfig } from "next";
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const nextConfig: import('next').NextConfig = {
+	sassOptions: {
+		includePaths: [path.join(__dirname, 'styles')],
+		prependData: `@import "index.scss";`,
+	},
+	typescript: {
+		ignoreBuildErrors: true,
+	},
+	async redirects() {
+		return [];
+	},
 };
 
 export default nextConfig;
