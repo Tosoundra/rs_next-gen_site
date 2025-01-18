feac0e225979fa3089978b635d0430e2116a1b2d
BURmister <olegcernysov903@gmail.com>
2025-01-18 23:48:48 +0300

    rework architecture && init styles

--- next.config.ts
+++ next.config.ts
@@ -5,16 +5,16 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const nextConfig: import('next').NextConfig = {
-	sassOptions: {
-		includePaths: [path.join(__dirname, 'styles')],
-		prependData: `@import "index.scss";`,
-	},
-	typescript: {
-		ignoreBuildErrors: true,
-	},
-	async redirects() {
-		return [];
-	},
+  // sassOptions: {
+  // 	includePaths: [path.join(__dirname, 'styles')],
+  // 	prependData: `@import "index.scss";`,
+  // },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  async redirects() {
+    return [];
+  },
 };
 
 export default nextConfig;
