81d615e724ee3161ccd1a3ce443be9713aeeab37
Klyamanin Maxim <maxim@sendev.gmbh>
2025-04-08 14:32:46 +0300

    - added translations for Contacts section

--- src/widgets/pages/home/contacts/ui/index.tsx
+++ src/widgets/pages/home/contacts/ui/index.tsx
@@ -3,10 +3,12 @@ import { LineAnimation } from '@/src/shared/line';
 import styles from './styles.module.scss';
 import { ComponentProps } from 'react';
 import { NetworkLogo } from '@shared';
+import { useLanguage } from '@/src/context/LanguageContext';
 
 type Props = ComponentProps<'div'>;
 
 const Contacts = ({ ...props }: Props) => {
+  const { translations } = useLanguage();
   return (
     <div {...props} className={`${styles.wrapper} content-wrapper flex flex-col`}>
       <div className={`${styles.network} flex flex-col`}>
@@ -17,7 +19,7 @@ const Contacts = ({ ...props }: Props) => {
             <NetworkLogo />
           </div> */}
           <div className={`${styles.networkItem} flex`}>
-            <h3 className={`${styles.caption} caption-38 text-boldest`}>Let&apos;s chat!</h3>
+            <h3 className={`${styles.caption} caption-38 text-boldest`}>{translations.contacts.chat}</h3>
           </div>
         </div>
         <div className={`${styles.networkRow} flex`}>
