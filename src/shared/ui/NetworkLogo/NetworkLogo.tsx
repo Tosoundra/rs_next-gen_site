1e938ece41cf9839a16f69fe91faf93e44f370be
Antony Kowalski <beheaded_96@mail.ru>
2025-05-07 17:26:00 +0300

    изменил блок КОНТАКТЫ

--- src/shared/ui/NetworkLogo/NetworkLogo.tsx
+++ src/shared/ui/NetworkLogo/NetworkLogo.tsx
@@ -1,14 +1,74 @@
+import React, { useState, useRef, memo } from 'react';
 import Image from 'next/image';
-import React from 'react';
 import styles from './styles.module.scss';
-type Props = { image: string };
 
-const NetworkLogo = ({ image }: Props) => {
+interface Props {
+  image: string;
+  imageOnHover: string;
+}
+
+const NetworkLogo = memo(({ image, imageOnHover }: Props) => {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
+    if (containerRef.current) {
+      const container = containerRef.current;
+      const rect = container.getBoundingClientRect();
+      const centerX = rect.left + rect.width / 2;
+      const centerY = rect.top + rect.height / 2;
+
+      const x = (e.clientX - centerX) / 10;
+      const y = (e.clientY - centerY) / 10;
+
+      setPosition({ x, y });
+    }
+  };
+
+  const handleMouseLeave = () => {
+    setPosition({ x: 0, y: 0 });
+  };
+
   return (
-    <div className={styles.container}>
-      <Image src={image} alt="network" />
+    <div
+      className={styles.container}
+      ref={containerRef}
+      onMouseMove={handleMouseMove}
+      onMouseLeave={handleMouseLeave}
+    >
+      <div
+        className={styles.wrapper}
+        style={{
+          transform: `translate(${position.x * 5}px, ${position.y * 5}px)`,
+          transition: 'transform 0.3s ease-out',
+        }}
+      >
+        {/* Обе картинки рендерятся одновременно, но меняется opacity */}
+        <Image
+          style={{
+            transform: `translate(${position.x * 5}px, ${position.y * 5}px)`,
+            transition: 'all 0.3s ease-out',
+          }}
+          className={`${styles.image} ${styles.imageDefault}`}
+          width={41}
+          height={35}
+          src={image}
+          alt="network"
+        />
+        <Image
+          style={{
+            transform: `translate(${position.x * 5}px, ${position.y * 5}px)`,
+            transition: 'all 0.3s ease-out',
+          }}
+          className={`${styles.image} ${styles.imageHover}`}
+          width={41}
+          height={35}
+          src={imageOnHover}
+          alt="network"
+        />
+      </div>
     </div>
   );
-};
-
+});
+NetworkLogo.displayName = 'NetworkLogo';
 export default NetworkLogo;
--- src/widgets/pages/home/contacts/ui/index.tsx
+++ src/widgets/pages/home/contacts/ui/index.tsx
@@ -4,6 +4,12 @@ import styles from './styles.module.scss';
 import { ComponentProps } from 'react';
 import { NetworkLogo } from '@shared';
 import { useLanguage } from '@/src/context/LanguageContext';
+import telegram from '/public/img/contacts/telegram.svg';
+import telegramHovered from '/public/img/contacts/telegram_hovered.svg';
+import vk from '/public/img/contacts/vk.svg';
+import vkHovered from '/public/img/contacts/vk_hovered.svg';
+import vector from '/public/img/contacts/vector.svg';
+import vectorHovered from '/public/img/contacts/vector_hovered.svg';
 
 type Props = ComponentProps<'div'>;
 
@@ -12,14 +18,16 @@ const Contacts = ({ ...props }: Props) => {
   return (
     <div {...props} className={`${styles.wrapper} content-wrapper flex flex-col`}>
       <div className={`${styles.network} flex flex-col`}>
-        <div className={`${styles.networkRow} flex`}>
-          {/* <div className={`${styles.networkItem} flex justify-end`}>
-            <NetworkLogo />
-            <NetworkLogo />
-            <NetworkLogo />
-          </div> */}
+        <div className={`${styles.networkRow}`}>
+          <div className={`${styles.networkItem} flex justify-end gap-12`}>
+            <NetworkLogo image={telegram} imageOnHover={telegramHovered} />
+            <NetworkLogo image={vk} imageOnHover={vkHovered} />
+            <NetworkLogo image={vector} imageOnHover={vectorHovered} />
+          </div>
           <div className={`${styles.networkItem} flex`}>
-            <h3 className={`${styles.caption} caption-38 text-boldest`}>{translations.contacts.chat}</h3>
+            <h3 className={`${styles.caption} caption-38 text-boldest`}>
+              {translations.contacts.chat}
+            </h3>
           </div>
         </div>
         <div className={`${styles.networkRow} flex`}>
