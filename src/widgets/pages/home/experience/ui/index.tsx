676569934946fcc30a483da56b97a9a1973fd48d
NikolayCharaev <hayyabusa_01@mail.ru>
2025-08-01 16:52:16 +0300

    fix

--- src/widgets/pages/home/experience/ui/index.tsx
+++ src/widgets/pages/home/experience/ui/index.tsx
@@ -43,139 +43,37 @@ const Experience = ({ ...props }: Props) => {
   return (
     <div {...props} ref={containerRef} className={styles.container}>
       <SpaceBackgroundZ className={`${styles.background}`} parentContainerRef={containerRef}/>
-      <section className="content-wrapper">
+      <section className="content-wrapper relative z-100 flex flex-col gap-20">
         {experienceList.map((experience, index) => (
           <section
             key={index}
-            className={`img-container${index === experienceList.length - 1 ? ' mb-20' : ''}`.replace('img-containermb-10', 'img-container mb-10')}
+            className={'h-screen flex items-center justify-center'}
           >
-            <Image
-              width={650}
-              height={650}
-              alt={getText(experience.title)}
-              loading="lazy"
-              src={experience.img}
-              className={`grayscale rounded-[30px] ${styles.image}`}
-            />
-            <span className="experience-title caption-120 font-black text-white">
-              {getText(experience.title)}
-            </span>
-            <div className="flex flex-col gap-2">
-              <span className="experience-subtitle caption-38 font-black text-blue">
-                {getText(experience.subtitle)}
-              </span>
-              <span className="experience-description text-[16px] md:text-[20px] font-normal text-white max-w-2xl mx-auto lg:mx-0">
-                {getText(experience.description)}
-              </span>
+            <div className=" flex flex-col lg:flex-row items-stretch gap-[90px]">
+              <Image
+                width={600}
+                height={600}
+                alt={getText(experience.title)}
+                loading="lazy"
+                src={experience.img}
+                className={`w-[600px] max-h-[600px] rounded-[30px] block flex-shrink-0`}
+              />
+              <div className="flex flex-col justify-between min-h-full flex-1 gap-10 5xl:gap-0">
+                <span className="experience-title caption-120 font-black text-white">
+                  {getText(experience.title)}
+                </span>
+                <span className="experience-subtitle caption-38 font-black text-blue">
+                  {getText(experience.subtitle)}
+                </span>
+                <span className="text-[16px] md:text-[20px] font-normal text-white mx-auto lg:mx-0">
+                  {getText(experience.description)}
+                </span>
+              </div>
             </div>
           </section>
         ))}
       </section>
-      <style jsx>{`
-        .img-container {
-          height: 110vh;
-          scroll-snap-align: start;
-          display: flex;
-          justify-content: start;
-          align-items: center;
-          position: relative;
-        }
-        .img-container span {
-          margin: 0;
-          position: absolute;
-          top: calc(35% - 25px);
-          left: calc(50% + 120px);
-        }
-
-        .img-container .experience-subtitle {
-          top: calc(46%);
-        }
-        
-        @media (max-width: 1023px) {
-          .img-container span {
-            top: calc(41% - 25px);
-          }
-        }
-
-        .img-container .experience-description {
-          top: calc(54%);
-        }
-
-        .experience-title.caption-120,
-        .experience-subtitle.caption-120,
-        .experience-description.caption-120 {
-          @media (max-width: 1919px) {
-            font-size: 92px;
-          }
-          @media (max-width: 1439px) {
-            font-size: 86px;
-          }
-          @media (max-width: 1279px) {
-            font-size: 58px;
-          }
-          @media (max-width: 1023px) {
-            font-size: 42px;
-          }
-          @media (max-width: 767px) {
-            font-size: 34px;
-          }
-          @media (max-width: 575px) {
-            font-size: 38px;
-          }
-          @media (max-width: 479px) {
-            font-size: 26px;
-          }
-          @media (max-width: 389px) {
-            font-size: 18px;
-          }
-        }
-
-        @media (max-width: 640px) {
-          .img-container {
-            justify-content: center;
-          }
-
-          .experience-title,
-          .experience-subtitle,
-          .experience-description {
-            left: 0% !important;
-          }
-
-          .experience-title {
-            top: calc(75%) !important;
-          }
-          .experience-subtitle {
-            top: calc(80%) !important;
-          }
-          .experience-description {
-            top: calc(85%) !important;
-
-            max-width: 90%;
-          }
-        }
-
-                @media (max-width: 443px) {
-                 .experience-title,
-          .experience-subtitle,
-          .experience-description {
-            left: 0% !important;
-          }
-
-          .experience-title {
-            top: calc(72%) !important;
-          }
-          .experience-subtitle {
-            top: calc(77%) !important;
-          }
-          .experience-description {
-            top: calc(82%) !important;
-
-            max-width: 90%;
-          }
-        }
-                
-                }
-      `}</style>
+      
     </div>
   );
 };
