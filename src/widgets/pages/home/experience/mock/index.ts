03209ffed1f5f475e871d5d5746b02316dba8c38
NikolayCharaev <hayyabusa_01@mail.ru>
2025-08-01 15:12:36 +0300

    add new images

--- src/widgets/pages/home/experience/mock/index.ts
+++ src/widgets/pages/home/experience/mock/index.ts
@@ -2,19 +2,19 @@ import { Experience } from '../types';
 
 export const experienceList: Experience[] = [
   {
-    img: '/img/mock/experience_1.png',
+    img: '/img/mock/new-1.gif',
     title: 'experience.0.title',
     subtitle: 'experience.0.subtitle',
     description: 'experience.0.description',
   },
   {
-    img: '/img/mock/experience_2.png',
+    img: '/img/mock/new-2.gif',
     title: 'experience.1.title',
     subtitle: 'experience.1.subtitle',
     description: 'experience.1.description',
   },
   {
-    img: '/img/mock/experience_3.png',
+    img: '/img/mock/new-3.gif',
     title: 'experience.2.title',
     subtitle: 'experience.2.subtitle',
     description: 'experience.2.description',
