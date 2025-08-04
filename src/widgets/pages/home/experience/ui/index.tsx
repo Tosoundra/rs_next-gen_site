62fd03ab97afb3f67479be428748211c0cfb3ff0
NikolayCharaev <hayyabusa_01@mail.ru>
2025-08-04 14:20:39 +0300

    fix

--- src/widgets/pages/home/experience/ui/index.tsx
+++ src/widgets/pages/home/experience/ui/index.tsx
@@ -58,7 +58,7 @@ const Experience = ({ ...props }: Props) => {
                 src={experience.img}
                 className={`w-[600px] max-h-[600px] rounded-[30px] block flex-shrink-0`}
               />
-              <div className="flex flex-col justify-between min-h-full flex-1 gap-2 lg:gap-10">
+              <div className="flex flex-col justify-between min-h-full flex-1 gap-2 lg:gap-10 w-[800px]">
                 <span className="experience-title caption-120 font-black text-white">
                   {getText(experience.title)}
                 </span>
